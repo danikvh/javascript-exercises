6a5a08d67cd04dcd01a5f27d75350d98a744ab66
danikvh <danivicen@gmail.com>
2022-07-21 10:57:13 +0200

    Exercise 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    let an2 = 0, an1 = 1, an = 1;
+    if (n === 0) return a0;
+    if (n === 1) return a1;
+    for (let i = 2; i <= n; i++) {
+        an = an1 + an2;
+        an2 = an1;
+        an1 = an;
+    }
+    return an;
 };
 
 // Do not edit below this line
