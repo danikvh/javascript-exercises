70177456ca72bcd71345e75d8293511428d0b7e3
danikvh <danivicen@gmail.com>
2022-07-21 10:35:37 +0200

    Exercise 9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const regex = /[^A-Za-z0–9]/g;
+    let cleanWord = word.toLowerCase().replace(regex, "");
+    let reverseWord = cleanWord.split("").reverse().join("");
+    return cleanWord === reverseWord;
 };
 
 // Do not edit below this line
